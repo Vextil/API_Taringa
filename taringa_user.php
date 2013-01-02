cf1a6a2d84bfcaecba512ddf2de024a53f50ceb4
Joaquin Cuitiño <vextil@gmail.com>
2013-01-02 03:49:28 -0200

    Fix
    Fixed the data fetching aswell as parsing and added the html compressing function.

--- taringa_user.php
+++ taringa_user.php
@@ -5,65 +5,56 @@ class taringa_user
 
 	private $data;
 	private $compressed_data;
-	public $user;
-	public $sex;
-	public $status;
+
+	public $usuario;
+	public $sexo;
+	public $estado;
 	public $karma;
-	public $rank;
-	public $rank_legacy;
-	public $score;
+	public $rango;
+	public $rango_pre_karma;
+	public $puntos;
 	public $posts;
-	public $topics;
-	public $comments;
-	public $followers;
-	public $following;
-	public $communities;
-	public $medals;
-	public $country;
-	public $message;
+	public $temas;
+	public $comentarios;
+	public $seguidores;
+	public $siguiendo;
+	public $comunidades;
+	public $medallas;
+	public $pais;
+	public $mensaje;
 	public $avatar;
-	public $website;
+	public $pagina_web;
 	public $facebook;
 	public $twitter;
 	
-	public function process($nick, $live = false)
+	public function process($nick)
 	{
-		$this->user = $nick;
-		if ($live) {
-			if ($this->fetchData()) {
-				$this->storeCache();
-				return true;
-			}
-			return false;
-		} else {
-			if (!$this->readCache()) {
-				if ($this->fetchData()) {
-					$this->storeCache();
-					return true;
-				}
-				return false;
-			}
-			return true;
-		}
+		$this->usuario = $nick;
+		$this->fetchData();
+		return $this->parseData();
 	}
 
-	public function fetchData()
+	private function fetchData()
 	{
-		if (!isset($this->data)) {
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, "http://www.taringa.net/{$this->user}");
-			curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; es-ES; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.3');
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-			$this->data = curl_exec($ch);
-			$this->compressed_data = compress::html($this->data);
-			curl_close ($ch); 
-		}
-		# GET USERNAME
+		$ch = curl_init();
+		$a = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; es-ES; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.3';
+		curl_setopt($ch, CURLOPT_URL, "http://www.taringa.net/{$this->usuario}");
+		curl_setopt($ch, CURLOPT_USERAGENT, $a);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		$this->data = curl_exec($ch);
+		$this->compressed_data = $this->compress($this->data);
+		curl_close ($ch); 
+	}
+
+	private function parseData()
+	{
+		// Username capitalizado correctamente
 		if (preg_match('/class="fn">@(.*)span>/', $this->data, $nick)) { 
 			$user = str_replace('</', '', $nick[1]); 
 			$continue = true;
 		} else { 
+
 			if (preg_match('/<span class="nickname">@(.*)<\/span> - <span class=\"adr\"/', $this->data, $nickk)) {
 				$user = $nickk[1]; 
 				$continue = true;
@@ -72,71 +63,72 @@ class taringa_user
 			}
 		}
 		if ($continue) {
-			# POSTS
+			// Posts
 			preg_match_all('/<a href="\/'.$user.'\/posts">(.*)<\/a>/', $this->data, $posts);
-			# COMMENTS
+			// Comentarios
 			preg_match('/Posts<\/span><\/li><li><strong>(.*)<\/strong><span>Comentarios/', $this->compressed_data, $comments);
-			# AVATAR
+			// Avatar
 			preg_match('/photo\" src=\"(.*)\" alt/', $this->data, $avatar);
-			# RANK
+			// Rango
 			preg_match('/<span class="role"><strong>(.*)<\/strong><\/span>/', $this->data, $rank);
-			# SCORE
+			// Puntos
 			preg_match('/<li><strong>(.*)<\/strong><span>Puntos/', $this->compressed_data, $score);
-			# FOLLOWERS
+			// Seguidores
 			preg_match('/class="data-followers-count" href="(.*)seguidores">(.*)<\/a>/', $this->data, $followers);
-			# FOLLOWING
+			// Siguiendo
 			preg_match('/class="data-following-count" href="(.*)siguiendo">(.*)<\/a>/', $this->data, $following);
-			# KARMA
+			// Karma
 			preg_match('/<strong>Nivel de Karma:<\/strong> (.*)<\/div>/', $this->data, $karma);
-			# TOPICS
+			// Temas
 			preg_match_all('/<a href="\/(.*)\/temas">(.*)<\/a>/', $this->data, $topics);
-			# WEBSITE
+			// Pagina web
 			preg_match('/<span class=\"url\"><a href=\"(.*)\" target=\"_blank\" rel=\"nofollow\"/', $this->compressed_data, $website);
-			# COMMUNITIES
+			$wbst = explode('"', $website[1]);
+			// Comunidades
 			preg_match_all('/\/comunidades">(.*)<\/a>/', $this->data, $comu);
 			if ($comu[1] > "0") { 
 				$communities = $comu[1][1]; 
 			} else { 
 				$communities = "0"; 
 			}
-			# MEDALS
+			// Medallas
 			preg_match('/medallas">(.*)<\/a>/', $this->data, $medals);
 			if ($medals[1] > 0){ 
 				$medal = $medals[1]; 
 			} else { 
 				$medal = "0"; 
 			}
-			# ONLINE STATUS
+			// Estado
 			preg_match('/-792px" title="(.*)"><\/span>/', $this->data, $status);
 			if ($status[1] == "Online"){ 
 				$current = "Online"; 
 			} else { 
 				$current = "Offline"; 
 			}
-			# PERSONAL MESSAGE
+			// Mensaje
 			if (preg_match('/<div><span class="bio">/', $this->compressed_data)) { 
 				$msg = null; 
 			} else { 
 				preg_match('/<div>(.*)<\/div>/', $this->data, $message); $msg = $message[1]; 
 			}
-			# FACEBOOK
+			// Facebook
 			if (preg_match('/<a href="http:\/\/facebook.com\/(.*)" target/', $this->data, $FB)) { 
 				$facebook = str_replace('profile.php?id=', '', $FB[1]); 
 			} else { 
 				$facebook = null; 
 			}
-			# TWITTER
+			// Twitter
 			if(preg_match('/<a href="http:\/\/twitter.com\/(.*)" target/', $this->data, $TW)){ 
 				$twitter = $TW[1]; 
 			} else { 
 				$twitter = null; 
 			}
-			# COUNTRY
+			// Pais
 			preg_match('/hspace="3" height="11" align="absmiddle" width="16" alt="(.*)" src=/', $this->data, $cntry);
 			$search = explode(",","ç,æ,œ,á,é,í,ó,ú,à,è,ì,ò,ù,ä,ë,ï,ö,ü,ÿ,â,ê,î,ô,û,å,e,i,ø,u");
 			$replace = explode(",","c,ae,oe,a,e,i,o,u,a,e,i,o,u,a,e,i,o,u,y,a,e,i,o,u,a,e,i,o,u");
 			$country = str_replace($search, $replace, $cntry[1]);
-			# SEX
+			// Sexo
 			if (preg_match('/icon hastipsy male/', $this->data)) { 
 				$sex = "hombre"; 
 			} elseif (preg_match('/icon hastipsy female/', $this->data)) { 
@@ -144,7 +136,7 @@ class taringa_user
 			} else { 
 				$sex = null; 
 			}
-			# RANK-LEGACY
+			// Rango pre-karma
 			if (preg_match('/medalla-new-full-user/', $this->data)) { 
 				$rank_legacy = "New Full User"; 
 			} elseif (preg_match('/medalla-full-user/', $this->data)) { 
@@ -155,24 +147,24 @@ class taringa_user
 				$rank_legacy = "Novato"; 
 			}
 		
-			$this->user = $user;
-			$this->sex = $sex;
-			$this->status = $current;
+			$this->usuario = $user;
+			$this->sexo = $sex;
+			$this->estado = $current;
 			$this->karma = $karma[1];
-			$this->rank = $rank[1];
-			$this->rank_legacy = $rank_legacy;
-			$this->score = str_replace( '.', '', $score[1]);
+			$this->rango = $rank[1];
+			$this->rango_pre_karma = $rank_legacy;
+			$this->puntos = str_replace( '.', '', $score[1]);
 			$this->posts = str_replace( '.', '', $posts[1][1]);
-			$this->topics = str_replace( '.', '', $topics[2][1]);
-			$this->comments = str_replace( '.', '', $comments[1]);
-			$this->followers = str_replace( '.', '', $followers[2]);
-			$this->following = str_replace( '.', '', $following[2]);
-			$this->communities = str_replace( '.', '', $communities);
-			$this->medals = str_replace( '.', '', $medal);
-			$this->country = $country;
-			$this->message = $message[1];
+			$this->temas = str_replace( '.', '', $topics[2][1]);
+			$this->comentarios = str_replace( '.', '', $comments[1]);
+			$this->seguidores = str_replace( '.', '', $followers[2]);
+			$this->siguiendo = str_replace( '.', '', $following[2]);
+			$this->comunidades = str_replace( '.', '', $communities);
+			$this->medallas = str_replace( '.', '', $medal);
+			$this->pais = $country;
+			$this->mensaje = $message[1];
 			$this->avatar = $avatar[1];
-			$this->website = $website[1];
+			$this->pagina_web = $wbst[0];
 			$this->facebook = $facebook;
 			$this->twitter = $twitter;
 
@@ -181,64 +173,13 @@ class taringa_user
 		return false;
 	}
 
-	private function storeCache()
+	/* Simple funcion para comprimir el HTML 
+	   y hacer el parseo de datos mas simple */
+	private function compress($html) 
 	{
-		$values = array(
-			array('nick', $this->user),
-			array('sex', $this->sex),
-			array('status', $this->status),
-			array('karma', $this->karma),
-			array('rank', $this->rank),
-			array('rank_legacy', $this->rank_legacy),
-			array('score', $this->score),
-			array('posts', $this->posts),
-			array('topics', $this->topics),
-			array('comments', $this->comments),
-			array('followers', $this->followers),
-			array('following', $this->following),
-			array('communities', $this->communities),
-			array('medals', $this->medals),
-			array('country', $this->country),
-			array('message', htmlspecialchars($this->message)),
-			array('avatar', $this->avatar),
-			array('website', $this->website),
-			array('facebook', $this->facebook),
-			array('twitter', $this->twitter),
-			array('last_update', time()),
-		);
-		db::insert('users_data', $values, $values);
-	}
-
-	private function readCache()
-	{
-		$db = db::row('SELECT * FROM users_data WHERE nick = :nick', array(array('nick', $this->user)));
-		if ($db) {
-			if ((time() - $db->last_update) < 3600) {
-				$this->user = $db->nick;
-				$this->sex = $db->sex;
-				$this->status = $db->status;
-				$this->karma = $db->karma;
-				$this->rank = $db->rank;
-				$this->rank_legacy = $db->rank_legacy;
-				$this->score = $db->score;
-				$this->posts = $db->posts;
-				$this->topics = $db->topics;
-				$this->comments = $db->comments;
-				$this->followers = $db->followers;
-				$this->following = $db->following;
-				$this->communities = $db->communities;
-				$this->medals = $db->medals;
-				$this->country = $db->country;
-				$this->message = $db->message;
-				$this->avatar = $db->avatar;
-				$this->website = $db->website;
-				$this->facebook = $db->facebook;
-				$this->twitter = $db->twitter;
-				return true;
-			}
-			return false;
-		}
-		return false;
+	    $search = array('/\>[^\S ]+/s', '/[^\S ]+\</s', '/(\s)+/s');
+	    $replace = array('>', '<', '\\1');
+	    return preg_replace($search, $replace, $html);
 	}
 
 }
